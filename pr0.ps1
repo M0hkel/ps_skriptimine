f9fec6eaa103c7a5f23605f839ac859c25d542c3
Mihkel Lepberg <mihkel.lepberg@voco.ee>
2024-11-11 10:53:29 +0200

    array add

---
+++ pr0.ps1
@@ -0,0 +1,11 @@
+﻿$Array1 = @(1,2,3)
+$Array2 = @(4,5,6)
+$Array3 = @(
+    ($Array1[0] + $Array2[0]),
+    ($Array1[1] + $Array2[1]),
+    ($Array1[2] + $Array2[2])
+)
+
+"" ; $Array1
+"" ; $Array2
+"" ; $Array3
