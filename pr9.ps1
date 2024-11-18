4c419ce22a80b52d20704cec00f1b082d9e83b27
Mihkel Lepberg <mihkel.lepberg@voco.ee>
2024-11-18 11:07:49 +0200

    Checks if notepad is running and how long it ran for if closed

---
+++ pr9.ps1
@@ -0,0 +1,15 @@
+﻿# sets the value to 0
+$i = 0
+
+# while the notepad is running prints that it is, 
+# if it stops doesn't print an error message
+while(Get-Process Notepad -ErrorAction SilentlyContinue){
+    Write-Host "Notepad is running"
+    # everytime the loop is ran increases the number
+    $i++ 
+    # waits 1 second before continuing to avoid spam
+    start-sleep -Seconds 1
+}
+
+# prints the amount of times the loop ran
+Write-Host "Notepad was running for $i seconds"
