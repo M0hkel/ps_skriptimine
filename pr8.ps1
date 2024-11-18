cc78fa71e909aee637d71439f8835976527cafb9
Mihkel Lepberg <mihkel.lepberg@voco.ee>
2024-11-18 10:42:59 +0200

    checks if Notepad is running

---
+++ pr8.ps1
@@ -0,0 +1,7 @@
+﻿# while the notepad is running prints that it is, 
+# if it stops doesn't print an error message
+while(Get-Process Notepad -ErrorAction SilentlyContinue){
+    Write-Host "Notepad is running"
+    # waits 1 second before continuing to avoid spam
+    start-sleep -Seconds 1
+}
