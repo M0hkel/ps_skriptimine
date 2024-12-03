95c19d4e97358f044834f83ab85ebbd03c3c1600
Mihkel Lepberg <mihkel.lepberg@voco.ee>
2024-12-03 10:48:07 +0200

    rohkem selgitusi

--- haldusylesanded/yl1.ps1
+++ haldusylesanded/yl1.ps1
@@ -12,17 +12,20 @@ $password = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
 # teeb uue kasutaja kasutades ennem tehtud kasutaja nime ja parooli
 New-LocalUser "$username" -Password $password -FullName "$eesnimi $perenimi" -Description "Local account - $username" -ErrorAction SilentlyContinue
 
-# vea tekkimisel kasutaja tegemisel 
+# vea tekkimisel kasutaja tegemisel kirjutab sõnumi
 if (-not $?) {
+    # kui kasutaja on olemas kirjutab vastava sõnumi
     if ($Error[0].Exception.Message -like "*already exists*") {
         Write-Host " "
         Write-Host "Kasutaja $username on juba olemas!"
     }
+    # muu vea korral annab teada, et on tekkinud viga
     else {
         Write-Host " "
         Write-Host "Viga kasutaja loomisel!"
     }
 }
+# kasutaja loomise õnnestumisel annab teada
 else {
     Write-Host " "
     Write-Host "Kasutaja $username edukalt loodud!"
